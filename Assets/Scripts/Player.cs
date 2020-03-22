11515e14100acc955dcb24708527bfd29f1da139
Chris Evans <chrisevans9629@gmail.com>
2020-03-22 15:39:51 -0500

    added spawner

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float Jump;
+    public float Speed;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,22 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        var vector = Vector3.zero;
+        if (Input.GetKey(KeyCode.D))
+        {
+            vector.x += Speed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            vector.x -= Speed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            vector.y += Jump;
+        }
+
         
+        rb.AddForce(vector * Time.deltaTime);
     }
 }
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float MinX;
+    public float MaxX;
+    public float Interval;
+    public GameObject Object;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Interval);
+            Instantiate(Object, transform.position + new Vector3(Random.Range(MinX,MaxX),0,0), Quaternion.identity, transform);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
