6f74d20182d787a276b9a387c0b1c71fef716854
Chris Evans <chrisevans9629@gmail.com>
2020-03-22 16:33:23 -0500

    adding bomb spawner

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,9 @@ public class Player : MonoBehaviour
     public float Jump;
     public float Speed;
     public Rigidbody rb;
+    public Vector3 Offset;
+    public float BombForce;
+    public GameObject Bomb;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +33,29 @@ public class Player : MonoBehaviour
         {
             vector.y += Jump;
         }
+        Vector3 mouse = Input.mousePosition;
+        //Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(
+        //    mouse.x,
+        //    mouse.y,
+        //    transform.position.y));
+        //Vector3 forward = mouseWorld - transform.position;
+        //transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+        //transform.LookAt(Input.mousePosition);
+        //Debug.Log(Input.mousePosition);
+        Debug.DrawLine(transform.position, transform.forward * 10);
+        if (Input.GetMouseButtonDown(0))
+        {
+           var bomb = Instantiate(Bomb, transform.position + Offset, transform.rotation);
 
+           //Vector3 mouse = Input.mousePosition;
+           //Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(
+           //    mouse.x,
+           //    mouse.y,
+           //    transform.position.y));
+           //Vector3 forward = mouseWorld - transform.position;
+
+            bomb.GetComponent<Rigidbody>().AddForce(transform.forward * BombForce, ForceMode.Impulse);
+        }
         
         rb.AddForce(vector * Time.deltaTime);
     }
