a6789411060759fceca5ad4f1379d33aa56eb518
Chris Evans <chrisevans9629@gmail.com>
2020-03-23 18:09:57 -0500

    adding angry birds level

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Camera Camera;
+    public float Speed;
+    public Transform Follow;
+
+    public Vector3 Offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Offset = Camera.transform.position - Follow.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Camera.transform.position = Vector3.MoveTowards(Camera.transform.position, Follow.position + Offset, Speed);
+    }
+}
