af5749f2c53629f6415b6f69ecfa70238acb6a0e
Chris Evans <chrisevans9629@gmail.com>
2020-04-11 20:29:51 -0500

    key door works

---
+++ Assets/Scripts/KeyDoor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyDoor : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Key"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
